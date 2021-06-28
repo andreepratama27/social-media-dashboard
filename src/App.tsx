4fe70ca9d7ba6e504a1502b5a04b1aae61d7a1b5
andreepratama27 <andre.pratama@accelbyte.net>
2021-06-28 21:29:20 +0700

    feat: init project with eslint, prettier & husky

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="App">
+      <p className="text-lg font-bold text-blue-500">
+        Welcome to Kumparan
+      </p>
+    </div>
+  );
+}
+
+export default App;
